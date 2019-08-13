0cd9204db1fa146d6e36632c32795203764c5e48
Hugo Fernandes <hugopakau@gmail.com>
2019-08-13 11:25:53 +0100

    Initial commit.

---
+++ app/src/main/java/com/pakau/crypto/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pakau.crypto;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+
+        textView.setText("oooo");
+        setContentView(textView);
+    }
+}
