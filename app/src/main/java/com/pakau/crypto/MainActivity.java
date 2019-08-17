d7e1f704ee3ebffa59f32548aed49170d6f4c703
Hugo Fernandes <hugopakau@gmail.com>
2019-08-17 13:00:57 +0100

    Read from URL using Okio.

--- app/src/main/java/com/pakau/crypto/MainActivity.java
+++ app/src/main/java/com/pakau/crypto/MainActivity.java
@@ -6,13 +6,15 @@ import android.util.Log;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+
+import okio.BufferedSource;
+import okio.Okio;
+import okio.Source;
 
 public class MainActivity extends Activity {
     private static final String IPIFY = "https://api.ipify.org?format=json";
@@ -31,21 +33,17 @@ public class MainActivity extends Activity {
         new Thread() {
             @Override
             public void run() {
-                StringBuilder stringBuilder = new StringBuilder();
                 try {
                     //String coins = "https://min-api.cryptocompare.com/data/all/coinlist"
                     long before = System.currentTimeMillis();
                     URL url = new URL(COINS);
                     URLConnection urlConnection = url.openConnection();
                     InputStream inputStream = urlConnection.getInputStream();
-                    BufferedReader bufferedReader =
-                            new BufferedReader(new InputStreamReader(inputStream));
-                    String s = bufferedReader.readLine();
-                    while (s != null) {
-                        stringBuilder.append(s);
-                        s = bufferedReader.readLine();
-                    }
-                    final String result = stringBuilder.toString();
+
+                    Source source = Okio.source(inputStream);
+                    BufferedSource bufferedSource = Okio.buffer(source);
+                    final String result = bufferedSource.readUtf8();
+
                     long after = System.currentTimeMillis();
                     Log.d("PAKAU", String.valueOf(after-before));
                     runOnUiThread(new Runnable() {
@@ -54,9 +52,6 @@ public class MainActivity extends Activity {
                             textView.setText(result);
                         }
                     });
-
-
-
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
